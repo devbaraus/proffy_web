f590c4e491868fdef37c67a45139d0e3ae2722b3
Bruno de Araujo Alves devbaraus <devbaraus@gmail.com>
2020-08-09 08:12:34 -0300

    initial
    Starting the project

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import './styles.css'
+import api from "../../services/api";
+
+export interface Teacher {
+    id: number
+    subject: string
+    cost: number
+    user_id: number
+    name: string
+    avatar: string
+    whatsapp: string
+    bio: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
+    return (
+        <article className="teacher-item">
+            <header>
+                <img
+                    src={teacher.avatar}
+                    alt={teacher.name}/>
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+            <p>
+                {teacher.bio}
+            </p>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a href={`https://wa.me/${teacher.whatsapp}`} target="_blank" onClick={createNewConnection}>
+                    <img src={whatsappIcon} alt="WhatsApp"/>
+                    Entrar em contato
+                </a>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,90 @@
+import React, {useEffect, useState} from "react";
+
+import PageHeader from "../../components/PageHeader";
+
+import "./styles.css"
+import TeacherItem, {Teacher} from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+import api from "../../services/api";
+
+function TeacherList() {
+    const [teachers, setTeachers] = useState([])
+
+    const [subject, setSubject] = useState('')
+    const [week_day, setWeekDay] = useState('')
+    const [time, setTime] = useState('')
+
+    useEffect(() => {
+        // if (subject && week_day && time) {
+        searchTeachers()
+        // }
+    }, [subject, week_day, time])
+
+    function searchTeachers() {
+        api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        }).then(response => {
+            const {data} = response
+            setTeachers(data)
+        })
+    }
+
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <Select
+                        label="Matéria"
+                        name="subject"
+                        options={[
+                            {value: 'Artes', label: 'Artes'},
+                            {value: 'Biologia', label: 'Biologia'},
+                            {value: 'Ciências', label: 'Ciências'},
+                            {value: 'Educação Física', label: 'Educação Física'},
+                            {value: 'Geografia', label: 'Geografia'},
+                            {value: 'História', label: 'História'},
+                            {value: 'Matemática', label: 'Matemática'},
+                            {value: 'Português', label: 'Português'},
+                            {value: 'Química', label: 'Química'},
+                        ]}
+                        value={subject}
+                        onChange={e => setSubject(e.target.value)}
+                    />
+                    <Select
+                        label="Dia da Semana"
+                        name="week_day"
+                        options={[
+                            {value: '0', label: 'Domingo'},
+                            {value: '1', label: 'Segunda-feira'},
+                            {value: '2', label: 'Terça-feira'},
+                            {value: '3', label: 'Quarta-feira'},
+                            {value: '4', label: 'Quinta-feira'},
+                            {value: '5', label: 'Sexta-feira'},
+                            {value: '6', label: 'Sábado'},
+                        ]}
+                        value={week_day}
+                        onChange={e => setWeekDay(e.target.value)}
+                    />
+                    <Input
+                        label="Hora"
+                        name="time"
+                        type="time"
+                        value={time}
+                        onChange={e => setTime(e.target.value)}
+                    />
+                </form>
+            </PageHeader>
+
+            <main>
+                {teachers.map((teacher: Teacher) => <TeacherItem key={teacher.id} teacher={teacher}/>)}
+            </main>
+        </div>
+    )
+}
+
+export default TeacherList;

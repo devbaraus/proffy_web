712afd858f50ed7d758056e2c4309e1f5657135d
Bruno de Araujo Alves devbaraus <devbaraus@gmail.com>
2020-08-12 21:12:17 -0300

    Updates

---
+++ precache-manifest.6fc6845930172f4a8da4b8626bd01c36.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd038df47bd5516f364a5c657f1697f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87213794fd47c5818dc0",
+    "url": "/static/css/main.57a7f411.chunk.css"
+  },
+  {
+    "revision": "f095c095cf7291e2ba4e",
+    "url": "/static/js/2.c3c7e201.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3c7e201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87213794fd47c5818dc0",
+    "url": "/static/js/main.2540f233.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "cfdf2f8f40f1fe362eb7dc03042b7483",
+    "url": "/static/media/camera.cfdf2f8f.svg"
+  },
+  {
+    "revision": "138ee05c2b65e2927cb92a157f1fda5f",
+    "url": "/static/media/done.138ee05c.svg"
+  },
+  {
+    "revision": "27ec98e27625babcc2717e8853fd425e",
+    "url": "/static/media/edit.27ec98e2.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "903b4056288464b2b15f0941710bf1a0",
+    "url": "/static/media/leave.903b4056.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "d8bfbfbab81e44ef9765419f1011a744",
+    "url": "/static/media/squared-background.d8bfbfba.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "11f6671e1d223509e17a26cbdfe5e415",
+    "url": "/static/media/success-background.11f6671e.svg"
+  },
+  {
+    "revision": "6dfaa2f94fc54e53666a511b779f90b4",
+    "url": "/static/media/warning.6dfaa2f9.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
